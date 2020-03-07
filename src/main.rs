8d8e432a18beed8906f8e6a8b585f62cbe88e006
Andrey Esaulov <esaulov.andrey@gmail.com>
2020-03-07 13:26:18 +0100

    test random

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate rand;
+
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+	let secret_number = rand::thread_rng().gen_range(1, 101);
+
+	println!("The secret is: {}", secret_number);
+
+	return;
+
+
+    println!("--- GUESS THE NUMBER ---");
+
+    println!("Please type your guess here: ");
+
+    let mut users_guess = String::new();
+
+    io::stdin().read_line(&mut users_guess)
+    	.expect("Failed to read the line");
+
+    println!("Your guess was: {}", users_guess);
 }
